0e52f9edd9870ac90aab712ab46e49892bbb3d60
luokaiii <1730185871@163.com>
2020-03-04 17:15:15 +0800

    feat: 列表页

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 import "./DetailsCard.less";
+import { Icon } from "antd";
 
 export default () => {
-  return <div className="details-card">详细信息卡片</div>;
+  return (
+    <div className="details-card">
+      <div className="dc1">
+        <div className="dc11">
+          <img
+            src="http://cdn.baletoo.cn/Uploads/housephoto/2958/2957860/oss_5dd77093bdfb5.png@!380_280.png"
+            alt=""
+          />
+        </div>
+        <div className="dc12">
+          <div className="title">
+            绿泉公寓-静安 近地铁 可做饭 新上架 电梯房 智能电表
+          </div>
+          <div className="model">
+            <Icon type="home" />
+            &nbsp; 合租|朝北-次卧|13㎡
+          </div>
+        </div>
+        <div className="dc13">￥300000</div>
+      </div>
+      <div className="dc2">
+        <div className="dc21">
+          <img
+            src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2841648446,236398816&fm=26&gp=0.jpg"
+            alt=""
+          />
+          <span className="name">孙先生</span>
+          <span className="tag">房东</span>
+        </div>
+        <div className="dc22">2020-03-04 发布</div>
+      </div>
+    </div>
+  );
 };
--- src/pages/frontend/list/index.jsx
+++ src/pages/frontend/list/index.jsx
@@ -1,5 +1,76 @@
 import React from "react";
+import { Form, Checkbox } from "antd";
+
+import DetailsCard from "../../../components/DetailsCard";
+import "./index.less";
 
 export default () => {
-  return <div>列表页</div>;
+  return (
+    <div className="list">
+      <div className="search-list">
+        <div className="title">
+          <span>区域找房</span>
+        </div>
+        <Form labelCol={{ span: 2 }} wrapperCol={{ span: 22 }}>
+          <Form.Item label="区域">
+            <Checkbox.Group>
+              <Checkbox>不限</Checkbox>
+              <Checkbox>宝山</Checkbox>
+              <Checkbox>浦东</Checkbox>
+              <Checkbox>青浦</Checkbox>
+              <Checkbox>虹口</Checkbox>
+              <Checkbox>珠海</Checkbox>
+              <Checkbox>嘉定</Checkbox>
+              <Checkbox>徐汇</Checkbox>
+              <Checkbox>长宁</Checkbox>
+              <Checkbox>静安</Checkbox>
+              <Checkbox>奉贤</Checkbox>
+              <Checkbox>金山</Checkbox>
+              <Checkbox>崇明</Checkbox>
+              <Checkbox>崇明</Checkbox>
+              <Checkbox>崇明</Checkbox>
+              <Checkbox>崇明</Checkbox>
+              <Checkbox>崇明</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item label="价格">
+            <Checkbox.Group>
+              <Checkbox>不限</Checkbox>
+              <Checkbox>1500以下</Checkbox>
+              <Checkbox>1500-3000</Checkbox>
+              <Checkbox>3000-6000</Checkbox>
+              <Checkbox>6000-10000</Checkbox>
+              <Checkbox>10000以上</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item label="类型">
+            <Checkbox.Group>
+              <Checkbox>不限</Checkbox>
+              <Checkbox>整租</Checkbox>
+              <Checkbox>合租</Checkbox>
+              <Checkbox>公寓</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item label="户型">
+            <Checkbox.Group>
+              <Checkbox>不限</Checkbox>
+              <Checkbox>一居室</Checkbox>
+              <Checkbox>两居室</Checkbox>
+              <Checkbox>三居室</Checkbox>
+              <Checkbox>四居室以上</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+          <hr color="#eee" />
+          <Form.Item label="排序">综合 | 价格↓ | 面积↓</Form.Item>
+        </Form>
+      </div>
+      <div className="content-list">
+        <DetailsCard />
+        <DetailsCard />
+        <DetailsCard />
+        <DetailsCard />
+        <DetailsCard />
+      </div>
+    </div>
+  );
 };
